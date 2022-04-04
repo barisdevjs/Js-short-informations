b60c9c92b9afa50ed1de402f5419708cbc4f8a0e
Barış SAVAŞ <barissavas17@gmail.com>
2022-04-04 20:42:31 +0300

    added

---
+++ Tricks/consoleLog1.js
@@ -0,0 +1,18 @@
+myFunc()
+var myFunc = function() {
+    let number = 30;
+    console.log('let' , number);
+}
+
+myFunc();
+function myFunc() {
+    var number = 50;
+    if (false) {
+        let number = 100;
+}
+    console.log('func',number);
+}
+myFunc();
+
+// 50
+// 30 30
